8d3302394b2cdf2cec1449ba6a8f8dae6545fb52
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-10-14 18:43:07 +0200

    feat: add HomePage component and integrate routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,17 @@
-import { useEffect } from "react";
-import { useStore } from "./store/useStore"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
 
 function App() {
 
-  const categories = useStore(s => s.categories);
-  const category = useStore(s => s.category);
-  const fetchCategories = useStore(s => s.fetchCategories);
-  const isLoading = useStore(s => s.isLoadingCategory);
-  const fetchCategoryById = useStore(s => s.fetchCategoryById);
-  const fetchTransactions = useStore(s => s.fetchTransactions);
-  const transactions = useStore(s => s.transactions);
-
-  useEffect(() => {
-    fetchCategories();
-    fetchTransactions({ startDate: '2025-10-01', endDate: '2025-10-31', baseCurrency: 'EUR' })
-  }, [fetchCategories, fetchTransactions]);
-
-
-  if (isLoading) return <>Caricamento...</>
-
   return (
-    <>
-      <ul>
-        {category && (
-          <li>
-            <h1 className="text-2xl">
-              {category.name}
-            </h1>
-            <p>{category.type}</p>
-          </li>
-        )}
-        {categories.map(c => (
-          <li key={c._id}>
-            <h1 className="text-2xl">
-              {c.name}
-            </h1>
-            <p>{c.type}</p>
-          </li>
-        ))}
-        <h2 className="text-3xl">Spese</h2>
-        {transactions.map(t => (
-          <li key={t._id}>
-            <h1 className="text-2xl">
-              {t.title}
-            </h1>
-            <p>{t.amount}</p>
-            <p>{t.amountInEUR}</p>
-          </li>
-        ))}
-      </ul>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route index Component={HomePage} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import { useStore } from "zustand"
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2 className='text-xl text-center'>
+                Lista movimenti
+            </h2>
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
+
+export default HomePage
