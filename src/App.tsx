545a03ef7d5c1c321ee63be790066c1c8c6a16da
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-10-24 19:20:00 +0200

    feat: add default layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { store } from "./store/store";
+import DefaultLayout from "./layouts/DefaultLayout";
+
 function App() {
 
-  return (
-    <>
+  const fetchCategories = store(s => (s.fetchCategories));
 
-    </>
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route Component={DefaultLayout}>
+          <Route index />
+          <Route path="/settings" />
+          <Route path="/categories" />
+          <Route path="/modify-categories" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
