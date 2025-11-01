8e38bb7d3be65d5fb91c6c918a1f6500d799610b
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-11-01 15:47:51 +0100

    upd: refactoring catch error for fetchCategories and fetchTransactions

---
+++ src/store/errorsSlice.ts
@@ -0,0 +1,28 @@
+import type { StateCreator } from "zustand";
+import type { TStore } from "../types/store";
+
+type TErrorsState = {
+    errorMessage: string | null
+}
+
+type TErrorsActions = {
+    setError: (message?: string) => void,
+    clearError: () => void
+}
+
+export type TErrorsSlice = TErrorsState & TErrorsActions;
+
+export const createErrorsSlice: StateCreator<
+    TStore,
+    [['zustand/immer', never]],
+    [],
+    TErrorsSlice
+> = (set) => ({
+    errorMessage: null,
+    setError: (message = 'Errore sconosciuto') => {
+        set({ errorMessage: message });
+    },
+    clearError: () => {
+        set({ errorMessage: null });
+    }
+})
--- src/types/store.ts
+++ src/types/store.ts
@@ -1,6 +1,11 @@
 import type { TCategoriesSlice } from "../store/categoriesSlice";
 import type { TFormSlice } from "../store/formSlice";
 import type { TModalSlice } from "../store/modalSlice";
-import type { TTransactionsSlice } from "../store/TransactionsSlice";
+import type { TTransactionsSlice } from "../store/transactionsSlice";
+import type { TErrorsSlice } from "../store/errorsSlice";
 
-export type TStore = TCategoriesSlice & TModalSlice & TFormSlice & TTransactionsSlice;
+export type TStore = TCategoriesSlice
+    & TModalSlice
+    & TFormSlice
+    & TTransactionsSlice
+    & TErrorsSlice;
